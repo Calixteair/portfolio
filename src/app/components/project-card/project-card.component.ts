75471d535e9b4d2892d684deae78dd811f86c4a8
calixteair <calixte.reymond@laposte.net>
2025-05-26 16:06:11 +0200

    test deploy

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,17 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project-card',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
 export class ProjectCardComponent {
-  @Input() project!: {
-    title: string;
-    technos: string[];
-    date: string;
-    context: string;
-  } | undefined;
+  @Input() project?: Project;
 
   hover = false;
 }
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {ProjectCardComponent} from '../../components/project-card/project-card.component';
 import {Project} from '../../models/project.model';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, CommonModule],
   standalone: true,
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
